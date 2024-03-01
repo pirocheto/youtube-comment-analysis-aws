5331974c507323a115fcd7c13d86d31bc7af1f93
pirocheto <pirocheto@gmail.com>
2024-03-01 17:08:44 +0100

    api gateway openapi

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,46 +1,56 @@
+data "template_file" "open_api" {
+  template = file("${path.module}/api.yml")
+  vars = {
+    url                     = "https://execute-api.${var.region}.amazonaws.com/dev"
+    integration_uri         = "arn:aws:apigateway:${var.region}:states:action/StartExecution"
+    integration_credentials = aws_iam_role.api_gateway_role.arn
+  }
+}
+
 resource "aws_api_gateway_rest_api" "api" {
   name        = var.api_name
   description = "API to create a report from YouTube comments"
+  body        = data.template_file.open_api.rendered
 
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "resource" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = "report"
-}
+# resource "aws_api_gateway_resource" "resource" {
+#   rest_api_id = aws_api_gateway_rest_api.api.id
+#   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+#   path_part   = "report"
+# }
 
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "method" {
+#   rest_api_id   = aws_api_gateway_rest_api.api.id
+#   resource_id   = aws_api_gateway_resource.resource.id
+#   http_method   = "POST"
+#   authorization = "NONE"
+# }
 
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS"
-  uri                     = "arn:aws:apigateway:${var.region}:states:action/StartExecution"
-  credentials             = aws_iam_role.api_gateway_role.arn
-  request_templates = {
-    "application/json" = <<-EOF
-      #set($input = $input.json('$'))
-      {
-        "input": "$util.escapeJavaScript($input)",
-        "stateMachineArn": "$util.escapeJavaScript($stageVariables.StateMachineArn)"
-      }
-      EOF
-  }
-}
+# resource "aws_api_gateway_integration" "integration" {
+#   rest_api_id             = aws_api_gateway_rest_api.api.id
+#   resource_id             = aws_api_gateway_resource.resource.id
+#   http_method             = aws_api_gateway_method.method.http_method
+#   integration_http_method = "POST"
+#   type                    = "AWS"
+#   uri                     = "arn:aws:apigateway:${var.region}:states:action/StartExecution"
+#   credentials             = aws_iam_role.api_gateway_role.arn
+#   request_templates = {
+#     "application/json" = <<-EOF
+#       #set($input = $input.json('$'))
+#       {
+#         "input": "$util.escapeJavaScript($input)",
+#         "stateMachineArn": "$util.escapeJavaScript($stageVariables.StateMachineArn)"
+#       }
+#       EOF
+#   }
+# }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on  = [aws_api_gateway_integration.integration]
+  # depends_on  = [aws_api_gateway_integration.integration]
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "dev"
   variables = {
@@ -49,23 +59,23 @@ resource "aws_api_gateway_deployment" "deployment" {
 }
 
 
-resource "aws_api_gateway_method_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = "200"
-}
+# resource "aws_api_gateway_method_response" "response_200" {
+#   rest_api_id = aws_api_gateway_rest_api.api.id
+#   resource_id = aws_api_gateway_resource.resource.id
+#   http_method = aws_api_gateway_method.method.http_method
+#   status_code = "200"
+# }
 
-resource "aws_api_gateway_integration_response" "response_200" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = aws_api_gateway_method_response.response_200.status_code
+# resource "aws_api_gateway_integration_response" "response_200" {
+#   rest_api_id = aws_api_gateway_rest_api.api.id
+#   resource_id = aws_api_gateway_resource.resource.id
+#   http_method = aws_api_gateway_method.method.http_method
+#   status_code = aws_api_gateway_method_response.response_200.status_code
 
-  response_templates = {
-    "application/json" = <<-EOF
-      #set ($parsedPayload = $util.parseJson($input.json('$')))
-      $parsedPayload
-      EOF
-  }
-}
+#   response_templates = {
+#     "application/json" = <<-EOF
+#       #set ($parsedPayload = $util.parseJson($input.json('$')))
+#       $parsedPayload
+#       EOF
+#   }
+# }
